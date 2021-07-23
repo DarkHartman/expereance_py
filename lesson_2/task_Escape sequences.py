3591313ba14fce4d63ae0665e3067ab33830e8b5
DarkHartman <jharlanov@gmail.com>
2021-07-23 23:32:27 +0300

    init

---
+++ lesson_2/task_Escape sequences.py
@@ -0,0 +1,12 @@
+table = "\\a-Bell (alert)\n" \
+        "\\b-Backspace\n" \
+        "\\n-New line\n" \
+        "\\t-Horizontal tab\n" \
+        "\\\-Backslash \\ \n" \
+        '\\\"-Double quotation mark"\n' \
+        "\\\'-Single quotation mark'"
+
+
+print("Escape sequences")
+print(table)
+
